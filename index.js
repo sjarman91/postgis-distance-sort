4cc93bdf3beacf0a482bd9ea8a9181c50a55ffd2
Shane Jarman <sjarman91@gmail.com>
2019-01-17 23:10:16 -0500

    Update index file

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const db = require('./models');
 
 // waiting for database connection
 db.sequelize.authenticate()
-  .then(() => db.Clinic.findAll())
-  .then((clinics) => {
-    console.log(clinics);
+  .then(() => db.Station.findAll())
+  .then((stations) => {
+    console.log(stations);
   })
 
 
--- seeders/20190117055410-station-list.js
+++ seeders/20190117055410-station-list.js
@@ -3,13 +3,10 @@ var stationsJSON = require('../citi-stations.json');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    // console.log(stationsJSON);
 
     const seedData = stationsJSON.map(data => {
       const { id, name, created_at, updated_at, geo } = data
       const coordinateString = `${geo.coordinates[1]} ${geo.coordinates[0]}`
-
-      console.log(coordinateString)
       return {
         id,
         name,
